c4a8a0c74951145f61082d469466382ba647e408
stefanlazarevic <stefanlaza97@gmail.com>
2023-06-06 14:56:32 -0700

    third commit

---
+++ memory_game/board.rb
@@ -0,0 +1,45 @@
+require_relative "card.rb"
+require "byebug"
+
+class Board
+    def initialize(size)
+        @board = Array.new(size) { Array.new(size) }
+        @size = size
+    end
+
+    attr_reader :size
+
+    def all_board_positions
+        board_positions = []
+
+        (0...@size).each do |i|
+            (0...@size).each do |j|
+                board_positions << [i, j]
+            end
+        end
+
+        board_positions
+    end
+
+    
+    def populate
+        board_positions = self.all_board_positions
+        
+        (size * size / 2).times do
+            random_position_1 = board_positions.sample
+            board_positions.delete(random_position_1)
+            random_position_2 = board_positions.sample
+            board_positions.delete(random_position_2)
+
+            temp = ("A".."Z").to_a.sample
+
+            @board[random_position_1[0]][random_position_1[1]] = Card.new(temp)
+            @board[random_position_2[0]][random_position_2[1]] = Card.new(temp)
+        end
+    end
+
+    
+end
+
+board = Board.new(4)
+board.populate
--- memory_game/card.rb
+++ memory_game/card.rb
@@ -1,26 +1,24 @@
 class Card
-    
-
     def initialize(face_value)
         @face_value = face_value
-        @face_up = false
-        
+        @face_up = false   
     end
-    
+
+    attr_reader :face_value, :face_up
     
     def hide
         @face_up = false
     end
+
     def reveal
         @face_up = true
+    end
 
-    end
     def to_s
-        face_value_string = @face_value.to_s
+        @face_up ? @face_value : " "
     end
+
     def ==(another_card)
         @face_value == another_card.face_value
-
     end
-
 end
