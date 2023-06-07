5d6d428345d44ae6ca72e1b1aa3ebec545925245
Manuel P <m.perezasensio@gmail.com>
2023-06-06 17:29:58 -0700

    final commit of the day

--- memory_game/game.rb
+++ memory_game/game.rb
@@ -5,6 +5,7 @@ class Game
     def initialize(size)
         if size.even?
             @board = Board.new(size)
+            @size = size
         else
             raise "Size must be an even number."
         end
@@ -12,31 +13,35 @@ class Game
         @board.populate
     end
 
-    attr_reader :board
+    attr_reader :board,  :size
 
     def play
         until @board.won?
             system("clear")
             @board.render
-            
-            puts "Enter a position with coordinates in the format [row, column] separated with a space like '1 2': "
-            first_guess = gets.chomp.split(" ").map(&:to_i)
-            raise "Invalid position" if first_guess.length > 2 || (0...@board.grid.length).include?([first_guess[0]]) || (0...@board.grid.length).include?([first_guess[0]])
-            @board.grid[first_guess[0]][first_guess[1]].reveal
-            @board.render
-            
-            puts "Enter another position with coordinates in the format [row, column] separated with a space like '1 2': "
-            second_guess = gets.chomp.split(" ").map(&:to_i)
-            raise "Invalid position" if second_guess.length > 2 || (0...@board.grid.length).include?([second_guess[1]]) || (0...@board.grid.length).include?([second_guess[1]])
-            @board.grid[second_guess[0]][second_guess[1]].reveal
-            @board.render
-            sleep(3)
+            begin
+                puts "Enter a position with coordinates in the format [row, column] separated with a space like '1 2': "
+                first_guess = gets.chomp.split(" ").map(&:to_i)
+                raise "Invalid position" if first_guess.length != 2 || first_guess[0] >= @size || first_guess[1] >= @size
+                @board.grid[first_guess[0]][first_guess[1]].reveal
+                @board.render
+                
+                puts "Enter another position with coordinates in the format [row, column] separated with a space like '1 2': "
+                second_guess = gets.chomp.split(" ").map(&:to_i)
+                raise "Invalid position" if second_guess.length != 2 || second_guess[0] >= @size || second_guess[1] >= @size
+                @board.grid[second_guess[0]][second_guess[1]].reveal
+                @board.render
+                sleep(3)
 
-            unless @board.grid[second_guess[0]][second_guess[1]].face_value == @board.grid[first_guess[0]][first_guess[1]].face_value
-                @board.grid[first_guess[0]][first_guess[1]].hide
-                @board.grid[second_guess[0]][second_guess[1]].hide
-            else
-                puts "Those cards match."
+                unless @board.grid[second_guess[0]][second_guess[1]].face_value == @board.grid[first_guess[0]][first_guess[1]].face_value
+                    @board.grid[first_guess[0]][first_guess[1]].hide
+                    @board.grid[second_guess[0]][second_guess[1]].hide
+                else
+                    puts "Those cards match."
+                end
+            rescue => error
+                puts error
+                retry
             end
         end
 
---
+++ memory_game/human_player.rb
@@ -0,0 +1,9 @@
+
+
+class Human_player
+    
+    def get_position
+        pos = gets.chomp.split(" ").map(&:to_i)
+    end
+
+end
